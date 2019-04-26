c9c8340aa22efd1f7eee8542563f52d57c4adf53
Tom <abaotom14@gmail.com>
2019-04-27 00:11:40 +0800

    add dashboard component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './layout/Header';
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <Header />
-    )
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
